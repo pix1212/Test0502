2c14f10c6e3f521a87a4cf588e3d8b389c1859a8
pix1212 <101629336+pix1212@users.noreply.github.com>
2022-05-03 15:41:07 +0900

    초기 커밋.

---
+++ test2/test2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+#define STACK_SIZE 100
+
+char stack[STACK_SIZE];
+int top = -1;
+
+
+void StackPush(char input)
+{
+	if (top >= (STACK_SIZE - 1))
+		return;
+
+	stack[++top] = input;
+}
+
+void StackPop()
+{
+	if (top = -1)
+		return;
+
+	return stack[top--];
+
+}
+
+void StackPrint()
+{
+	int i;
+
+	for (i = 0; i <= top; i++)
+		cout << i << " ";
+}
+
+bool StackisFull()
+{
+	if (top = -1)
+		return true;
+}
+
+
+int main()
+{
+
+	return 0;
+}
---
+++ test3/test3.cpp
@@ -0,0 +1,32 @@
+﻿// test2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int fibo(int a, int b)
+{
+	if (a > 100)
+	{
+		return 0;
+	}
+
+	int sum = a + b;
+
+	cout << a << " ";
+
+	fibo(b, sum);
+
+
+}
+int main() {
+
+	int x = 0;
+	int y = 1;
+
+	fibo(x, y);
+
+	return 0;
+}
